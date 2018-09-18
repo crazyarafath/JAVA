f2eab079d4d2b494be78d2bea2c82793ab5dc3f2
crazyarafath <42993466+crazyarafath@users.noreply.github.com>
2018-09-18 15:32:43 +0530

    Create leap.java

---
+++ leap.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Char
+{
+	public static void main(String[ ] arg)
+	{
+	int i;
+	Scanner sc=new Scanner(System.in);
+	i=sc.nextInt();
+	if(i%4==0){
+		System.out.println("yes");}
+	else{
+			System.out.println("no");}
+		
+	
+	}
+}
+	
