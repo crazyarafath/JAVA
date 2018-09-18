a714f231ca5059568df968ff58aeee09725a249d
crazyarafath <42993466+crazyarafath@users.noreply.github.com>
2018-09-18 14:56:57 +0530

    Create alphabet.java

---
+++ alphabet.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Char
+{
+	public static void main(String[ ] arg)
+	{
+	int i=0;
+	Scanner sc=new Scanner(System.in);
+	char ch=sc.next( ).charAt(0);	 
+            if((ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
+		System.out.println("Alphabet");}
+		else{
+		System.out.println("No");	}	
+	}
+}
