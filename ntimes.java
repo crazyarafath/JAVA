3598bd487f1ff4270ad58db5b012c5253fd7b8db
crazyarafath <42993466+crazyarafath@users.noreply.github.com>
2018-09-18 15:43:14 +0530

    Create ntimes.java

---
+++ ntimes.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class Char
+{
+	public static void main(String[ ] arg)
+	{
+	int i;
+	Scanner sc=new Scanner(System.in);
+	i=sc.nextInt();
+	for (int j = 0; j < i; j++) {
+		 System.out.println("Hello");
+	}
+	
+	}
+}
+	
