01beb6d27c189f9624b254157a181ee2c6c421de
crazyarafath <42993466+crazyarafath@users.noreply.github.com>
2018-09-18 16:05:21 +0530

    Create sumnat.java

---
+++ sumnat.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Char
+{
+	public static void main(String[ ] arg)
+	{
+	int i;
+	Scanner sc=new Scanner(System.in);
+	i=sc.nextInt();
+	int sum=0;
+	while(i>0) {
+		sum+=i;
+		i-=1;}
+            System.out.println(sum);
+		 
+	
+	
+	}
+}
+	
