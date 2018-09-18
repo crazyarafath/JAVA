5c179656ea780eba2de85eff3cc9f111fb2c4513
crazyarafath <42993466+crazyarafath@users.noreply.github.com>
2018-09-18 16:28:15 +0530

    Create suma.java

---
+++ suma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Char
+{
+	public static void main(String[ ] arg)
+	{
+	int N,K;
+	int a=0;
+	Scanner sc=new Scanner(System.in);
+	int[] array=new int[a];
+	a=sc.nextInt();
+	N=sc.nextInt();
+	K=sc.nextInt();
+	int sum=0;
+	while(K>0) {
+		sum+=K;
+		K-=1;}
+            System.out.println(sum);
+		 
+	
+	
+	}
+}
+	
